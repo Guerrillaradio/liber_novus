cd2c10a0653b3463193cd11aed7b1a71c71262f2
Guerrillaradio <104956986+Guerrillaradio@users.noreply.github.com>
2026-05-10 17:05:20 -0400

    Update game.js

--- game.js
+++ game.js
@@ -4,15 +4,90 @@ let current = null;
 
 const places = [
   {
-    image: "assets/images/chile.jpg",
-    continent: "américa",
+    image: "assets/images/Captura de pantalla 2024-12-31 003926.png",
+    continent: "oceania",
+    country: "australia"
+  },
+  {
+    image: "assets/images/Captura de pantalla 2025-09-06 213006.png",
+    continent: "europa",
+    country: "grecia"
+  },
+  {
+    image: "assets/images/Captura de pantalla 2025-09-06 213935.png",
+    continent: "america",
+    country: "peru"
+  },
+  {
+    image: "assets/images/Captura de pantalla 2025-09-25 191932.png",
+    continent: "asia",
+    country: "japon"
+  },
+  {
+    image: "assets/images/Captura de pantalla 2025-09-25 210135.png",
+    continent: "europa",
+    country: "grecia"
+  },
+  {
+    image: "assets/images/Captura de pantalla 2025-09-25 210302.png",
+    continent: "europa",
+    country: "grecia"
+  },
+  {
+    image: "assets/images/Captura de pantalla 2025-09-25 210500.png",
+    continent: "europa",
+    country: "italia"
+  },
+  {
+    image: "assets/images/Captura de pantalla 2025-09-25 211002.png",
+    continent: "europa",
+    country: "italia"
+  },
+  {
+    image: "assets/images/Captura de pantalla 2025-09-25 211212.png",
+    continent: "africa",
+    country: "tunez"
+  },
+  {
+    image: "assets/images/Captura de pantalla 2025-09-25 211403.png",
+    continent: "africa",
+    country: "tunez"
+  },
+  {
+    image: "assets/images/Captura de pantalla 2025-09-25 211837.png",
+    continent: "europa",
+    country: "Barcelona"
+  },
+  {
+    image: "assets/images/Captura de pantalla 2025-09-25 191932.png",
+    continent: "asia",
+    country: "japon"
+  },
+  {
+    image: "assets/images/Captura de pantalla 2025-09-25 191932.png",
+    continent: "asia",
+    country: "japon"
+  },
+  {
+    image: "assets/images/Captura de pantalla 2025-11-16 041031.png",
+    continent: "europa",
+    country: "italia"
+  },
+  {
+    image: "assets/images/Captura de pantalla 2025-12-26 163429.png",
+    continent: "america",
     country: "chile"
   },
   {
-    image: "assets/images/japan.jpg",
+    image: "assets/images/Captura de pantalla 2025-12-26 201414.png",
     continent: "asia",
-    country: "japón"
-  }
+    country: "taiwan"
+  },
+  {
+    image: "assets/images/Captura de pantalla 2026-01-17 213900.png",
+    continent: "asia",
+    country: "arabia saudita"
+  },
   // puedes añadir más
 ];
 
